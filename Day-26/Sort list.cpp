35db8d061450c0d3f48089a0b3f9d5b18eb2b31c
jatin-shrm <jsharma3182002@gmail.com>
2024-09-09 00:26:34 +0530

    Update: Time complexity

--- Day-26/Sort list.cpp
+++ Day-26/Sort list.cpp
@@ -12,26 +12,64 @@ struct ListNode
 class Solution
 {
 public:
+    ListNode *divide(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode *merger(ListNode *l1, ListNode *l2)
+    {
+        ListNode *temp = new ListNode(-1);
+        ListNode *dummy = temp;
+        while (l1 && l2)
+        {
+            if (l1->val <= l2->val)
+            {
+                dummy->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                dummy->next = l2;
+                l2 = l2->next;
+            }
+            dummy = dummy->next;
+        }
+        while (l1)
+        {
+            dummy->next = l1;
+            l1 = l1->next;
+            dummy = dummy->next;
+        }
+        while (l2)
+        {
+            dummy->next = l2;
+            l2 = l2->next;
+            dummy = dummy->next;
+        }
+        return temp->next;
+    }
+    ListNode *mergeSort(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+
+        ListNode *middle = divide(head);
+        ListNode *right = middle->next;
+        middle->next = NULL;
+        ListNode *left = head;
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merger(left, right);
+    }
     ListNode *sortList(ListNode *head)
     {
-        if (head == NULL || head->next == NULL)
-        {
-            return head;
-        }
-        ListNode *p = head;
-        vector<int> v;
-        while (p != NULL)
-        {
-            v.push_back(p->val);
-            p = p->next;
-        }
-        p = head;
-        sort(v.begin(), v.end());
-        for (auto it : v)
-        {
-            p->val = it;
-            p = p->next;
-        }
-        return head;
+        return mergeSort(head);
     }
 };
