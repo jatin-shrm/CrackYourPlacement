1f48ddd5aa29012cec3e7f3532f17c21f52d8e3e
jatin-shrm <jsharma3182002@gmail.com>
2024-09-17 17:23:44 +0530

    Find total unique binary search tree

---
+++ Day-36/Find total unique binary tree.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    vector<TreeNode *> solve(int start, int end)
+    {
+        if (start > end)
+            return {NULL};
+        if (start == end)
+        {
+            TreeNode *temp = new TreeNode(start);
+            return {temp};
+        }
+        vector<TreeNode *> result;
+        for (int i = start; i <= end; i++)
+        {
+            vector<TreeNode *> LeftBst = solve(start, i - 1);
+            vector<TreeNode *> RightBst = solve(i + 1, end);
+
+            for (TreeNode *leftRoot : LeftBst)
+            {
+                for (TreeNode *rightRoot : RightBst)
+                {
+                    TreeNode *root = new TreeNode(i);
+                    root->left = leftRoot;
+                    root->right = rightRoot;
+                    result.push_back(root);
+                }
+            }
+        }
+        return result;
+    }
+    vector<TreeNode *> generateTrees(int n)
+    {
+        return solve(1, n);
+    }
+};
